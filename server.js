3ec470a5599659468e8cb3398a621cfce838f614
Dezolf <60351006+Dezolf34@users.noreply.github.com>
2025-11-09 22:53:22 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,14 +1,6 @@
-const express = require("express");
-const server = express();
+var http = require('http');
 
-server.all("/", (req, res) => {
-    res.send("Bot this started!");
-})
-
-function keepAlive() {
-    server.listen(3000, () => {
-        console.log("Server is Ready!");
-    });
-}
-
-module.exports = keepAlive;
+http.createServer(function (req, res) {
+  res.write("I'm alive");
+  res.end();
+}).listen(8080);
